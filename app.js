a4f93a159d54d12f9ca658d240dbe3a49806e61f
Sarah Tirey <sarah@email.com>
2021-01-12 15:22:42 -0500

    committing user authentication & cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var cors = require('cors')
 let express = require("express");
 const app = express();
 const sequelize = require("./db");
@@ -10,9 +11,9 @@ let announcement = require('./controllers/announcementcontroller.js')
 
 
 sequelize.sync();
-
+app.use(cors())
 app.use(express.json());
-
+app.use(require('./middleware/headers'));
 /*********************
  ****EXPOSED ROUTE****
  ********************/
@@ -29,6 +30,4 @@ app.use('/announcement', announcement)
 
 
 
-app.listen(3000, function() {
-    console.log('App is listening on port 3000')
-})
+app.listen(process.env.PORT, () => console.log(`App is listening on ${process.env.PORT}`));
--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,34 +1,22 @@
 const jwt = require('jsonwebtoken');
 const User = require('../db').import('../models/user');
 //this files checks if the request has a token attached
-const validateSession = (req, res, next) => {
-    const token = req.headers.authorization;
-    console.log('token -->', token);
-    if(!token) {
-        return res.status(403).send({ auth: false, message: "No token provided"})
-    } else {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
-            console.log('decodeToken -->', decodeToken);
-            if (!err && decodeToken) {
-                User.findOne({
-                    where: {
-                        id: decodeToken.id
-                    }
-                })
-                .then(user => {
-                    console.log('user -->', user);
-                    if (!user) throw err;
-                    console.log('reg -->', req);
-                    req.user = user;
-                    return next();
-                })
-                .catch(err => next(err));
-            } else {
-                req.errors = err;
-                return res.status(500).send('Not Authorized');
-            }
-        });
-    }
-};
 
-module.exports = validateSession;
+ 
+module.exports = async (req, res, next) => {
+    const token= req.headers.authorization; //Postman --headers (key)Authorization
+ 
+    //try this code & if it doesn't work catch the error and run this other code
+    try{
+        const decoded= await jwt.verify(token, process.env.JWT_SECRET)
+        const user= await User.findOne({where: {id: decoded.id}});
+        if(!user) throw new Error('no user found'); 
+        req.user= user; 
+        next();
+ 
+    }catch(err){
+        res.status(500).json({error:err});
+    }
+ 
+
+}
